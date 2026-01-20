a5981d990238945cb97f13b498b80350e5d10b64
Turgunov Dostonbek <turgunovdostonbek@gmail.com>
2026-01-20 10:45:44 +0500

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer__container container">
-        
+        <a className="footer__link" href="#">
+          Turgunov Dostonbek....
+        </a>
       </div>
     </div>
   );
